289030a10e406b17ffb001d3c7e2911b96217b13
Tat-Dem <innet06@gmail.com>
2025-02-05 11:23:24 +0200

    TD-HW11 | get prettier,improving the positive test and adding negative tests to the file Login.spec.ts

--- tests/Login.spec.ts
+++ tests/Login.spec.ts
@@ -21,28 +21,25 @@ test.describe('Login tests', async () => {
       expect(jwt).toMatch(/^eyJ[a-zA-Z0-9-_]+\.[a-zA-Z0-9-_]+\.[a-zA-Z0-9-_]+$/)
     }
   })
-  test('Incorrect HTTP method', async ({ request }) => {
-    const response = await request.post('https://backend.tallinn-learning.ee/login/student', {
-      data: LoginDTO.createLoginWithCorrectData(),
-    })
-    expect(response.status()).toBe(StatusCodes.UNAUTHORIZED)
-  })
   test('Unsuccessful authorization with GET method', async ({ request }) => {
     const response = await request.get('https://backend.tallinn-learning.ee/login/student', {
       data: LoginDTO.createLoginWithCorrectData(),
     })
+    console.log(await response.text());
     expect(response.status()).toBe(StatusCodes.METHOD_NOT_ALLOWED)
   })
   test('Unsuccessful authorization with PUT method', async ({ request }) => {
     const response = await request.put('https://backend.tallinn-learning.ee/login/student', {
       data: LoginDTO.createLoginWithCorrectData(),
     })
+    console.log(await response.text());
     expect(response.status()).toBe(StatusCodes.METHOD_NOT_ALLOWED)
   })
   test('Unsuccessful authorization with DELETE method', async ({ request }) => {
     const response = await request.delete('https://backend.tallinn-learning.ee/login/student', {
       data: LoginDTO.createLoginWithCorrectData(),
     })
+    console.log(await response.text());
     expect(response.status()).toBe(StatusCodes.METHOD_NOT_ALLOWED)
   })
 })
